4162ecf3a436340460e0d08d532de850c79b1c55
aashimasingh <aashimasingh1995@gmail.com>
2017-10-13 08:45:09 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -8,7 +8,7 @@ import string
 import re
 import pandas as pd
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 train_x = pd.read_csv('train_set_x.csv',encoding="utf-8")
 train_y = pd.read_csv('train_set_y.csv')
 print(train_x.size)
@@ -22,9 +22,11 @@ print(train_x.size)
 #remove words starting with https
 train_x['Text'] = train_x['Text'].str.replace('https\w+', '')
 
-train_x['Text'] = train_x['Text'].str.replace('½','')
-train_x['Text'] = train_x['Text'].str.replace('¾','')
-train_x['Text'] = train_x['Text'].str.replace('¼','')
+#remove fractions
+train_x['Text'] = train_x['Text'].str.replace(u'[\u00BC\u00BE\u00BD]','')
+#remove superscripts
+train_x['Text'] = train_x['Text'].str.replace(u'[\xb9\xb2\xb3\u2070]','')
+
 train_x['Text'] = train_x['Text'].str.replace(r'([^\s\w]|_)+','')
 #removing non-latin characters
 train_x['Text'] = train_x['Text'].str.replace(u'[^\x00-\x7F\x80-\xFF\u0100-\u017F\u0180-\u024F\u1E00-\u1EFF]','')
@@ -34,9 +36,15 @@ train_x['Text'] = train_x['Text'].str.replace(u'[^\x00-\x7F\x80-\xFF\u0100-\u017
 train_x['Text'] = train_x['Text'].apply(lambda col: col.strip())
 
 merged_df = pd.merge(train_y,train_x,on='Id')
+#new train y with fewer rows
+new_train_y = merged_df.iloc[:,:2]
 
-vectorizer = TfidfVectorizer(analyzer='char')
+vectorizer = CountVectorizer(analyzer='char')
 X = vectorizer.fit_transform(train_x['Text'].values).toarray()
 print(X.shape)
 print(vectorizer.get_feature_names());
-np.savetxt("id_vector.csv",X,delimiter=",")
+
+#add column of ID to 2D array
+col = np.array(train_x['Id'])
+Y = np.column_stack((col,X))
+np.savetxt("id_vector.csv",Y,delimiter=",")
